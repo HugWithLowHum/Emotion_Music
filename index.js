22d5d4df670cfcba6ef6ea4528278596b600764b
HugWithLowHum <150521768+HugWithLowHum@users.noreply.github.com>
2025-11-23 15:20:17 -0500

    Finished core front-end construction.
    Finished essential front-end constructions:
    1. Centered title, Background section and coords section.
    2. Dot on the Circumplex now has intuitive coords and follows the mouse click.
    
    NEXT STEP: Adding music features to the webpage, using Tone.js.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 //window.alert(`This is an alert!`);
-let x_coord;
-let y_coord;
-document.getElementById('coords_submit').onclick = function(){
-    x_coord = document.getElementById('x_input').value;
-    y_coord = document.getElementById('y_input').value;
-    x_coord = Number(x_coord);
-    y_coord = Number(y_coord);
-    console.log(`The emotion coordinates are: (${x_coord}, ${y_coord})`)
-    document.getElementById('current_coords_out').textContent = `Your current emotion coordinates are: (${x_coord}, ${y_coord})`;
-    console.log(typeof x_coord, typeof y_coord);
-}
+const dot = document.getElementById('dot');
+const output = document.getElementById('currentCoordsOut');
+coordsSystem.addEventListener('click', function(e){
+    //e for "event", i.e. click event.
+    const rect = coordsSystem.getBoundingClientRect();
+    //x and y are coords relative to the image
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    //The intuitive (0,0), which is the center of the image, relative to the image
+    const center_y = (rect.bottom-rect.top)/2;
+    const center_x = (rect.right-rect.left)/2;
+    //intuitive_x and intuitive_y are the intuitive coords with intuitive (0,0)
+    const intuitive_x = Number(((x - center_x)/5).toFixed(2));
+    const intuitive_y = Number((-(y - center_y)/5).toFixed(2));
+    // Show the dot on the image
+    dot.style.left =`${x}px`; 
+    dot.style.top = `${y}px`;
+    dot.style.display = "block";
+    // Display the coordinates
+    output.textContent = `Your current emotion coordinates are: (${intuitive_x}, ${intuitive_y})`;
+})
